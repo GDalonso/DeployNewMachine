252bc6b6df39ed7691705aeadbc0264418593e86
Gui Dalonso <guilherme.castro@fatec.sp.gov.br>
2019-02-23 19:12:15 -0300

    ssh update

--- DeploySetup.sh
+++ DeploySetup.sh
@@ -11,6 +11,9 @@ echo key start
 echo
 cat /home/$USER/.ssh/id_rsa.pub
 echo Copy and add the ssh key to your github and gitlab
+echo go to this url https://medium.com/codebase/configuring-ssh-key-for-github-9d2416a377ae
+echo finish the configuration
+echo it only works with ssh cloned repos
 echo
 echo https://github.com/settings/keys
 echo https://gitlab.com/profile/keys
